ac4126fcffeffb29f81b8b9a4fbebd649352e5d7
Wasabi <admin@factorium1.de>
2025-10-10 12:43:42 +0200

    chore(web): setup main functionallity for nextjs

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Home',
+}
+
 const Homepage = () => {
   return <p>Hello there</p>
 }
---
+++ apps/web/src/lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Pulse'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A real-time survey app'
+export const APP_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
