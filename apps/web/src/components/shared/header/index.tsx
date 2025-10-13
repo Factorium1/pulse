1750e7d9b0fadfb8243254cd4632c31bb663cd09
Wasabi <admin@factorium1.de>
2025-10-13 22:20:08 +0200

    fix(navbar): fix styling of navbar so it fits with the landing page

--- apps/web/src/components/shared/header/index.tsx
+++ apps/web/src/components/shared/header/index.tsx
@@ -3,7 +3,7 @@ import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div className="sticky top-10 z-10 w-sm flex-between backdrop-blur-sm bg-opacity-30 bg-white/30 border border-gray-300 md:w-lg mx-auto px-5 py-3 rounded-full ">
+    <div className="min-w-[300px] absolute left-1/2 top-10 z-10 transform -translate-x-1/2 flex-between backdrop-blur-sm bg-opacity-30 bg-white/30 border border-gray-300 md:w-lg px-5 py-3 rounded-full">
       <h1 className="text-2xl font-bold">Pulse</h1>
       <button className="md:hidden p-2 rounded-full hover:bg-gray-200 transition">
         <FaBars />
