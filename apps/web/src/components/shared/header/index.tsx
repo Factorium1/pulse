04bc80470e918f170aa153c906d430199c5f8193
Wasabi <admin@factorium1.de>
2025-10-13 14:30:40 +0200

    style(navbar): Improve navigatoin on mobile devices

--- apps/web/src/components/shared/header/index.tsx
+++ apps/web/src/components/shared/header/index.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className="backdrop-blur-sm bg-opacity-30 bg-white/30 border border-gray-300 flex-between md:w-lg mx-auto mt-5 px-5 py-3 position-sticky rounded-full top-4 z-10">
+    <div className="hidden backdrop-blur-sm bg-opacity-30 bg-white/30 border border-gray-300 md:flex-between md:w-lg mx-auto mt-5 px-5 py-3 rounded-full">
       <h1 className="text-2xl font-bold">Pulse</h1>
       <nav className="flex-center space-x-4 text-md">
         <Link href="/">Funktionen</Link>
         <Link href="/">Preise</Link>
         <Link href="/">Download</Link>
-        <Link href="/">Login</Link>
+        <Link href="/" className="font-bold">
+          Login
+        </Link>
       </nav>
     </div>
   )
