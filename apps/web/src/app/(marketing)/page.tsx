841fce12f0e3d5417941ddfd26728a888e6382eb
Wasabi <admin@factorium1.de>
2025-10-13 22:19:38 +0200

    feat(page): create usefull home section

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -3,7 +3,18 @@ export const metadata = {
 }
 
 const Homepage = () => {
-  return <p>Hello there</p>
+  return (
+    <section className="[min-height:calc(100svh-40px)] md:[min-height:calc(100vh-40px)] flex-center">
+      <div className="flex-col gap-6 text-center text-white">
+        <h1 className="h1-bold">Welcome to Pulse</h1>
+        <p className="max-w-2xl text-lg text-white/70">
+          Craft powerful, beautiful surveys that capture authentic responses — then transform raw
+          feedback into real‑time analytics, actionable insights, and data‑driven decisions to grow
+          your product, engage your audience, and guide your roadmap.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Homepage
