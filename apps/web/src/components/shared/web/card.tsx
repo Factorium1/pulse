69816dc84743991b6e93c87660e7775711443bc0
Wasabi <admin@factorium1.de>
2025-10-14 17:28:52 +0200

    style(card): fix cards not wrapping on md screens

--- apps/web/src/components/shared/web/card.tsx
+++ apps/web/src/components/shared/web/card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from '@/types/cardprops'
 
 export const Card = ({ icon: Icon, title, description }: CardProps) => {
   return (
-    <div className="border rounded-lg p-6 shadow-lg hover:shadow-xl transition flex flex-col gap-4 md:min-w-md">
+    <div className="border rounded-lg p-6 shadow-lg hover:shadow-xl transition flex flex-col gap-4 lg:min-w-md">
       <h3 className="font-bold h3-bold flex items-center gap-2">
         {Icon && (
           <span className="bg-indigo-200 p-4 rounded-full flex-center">
