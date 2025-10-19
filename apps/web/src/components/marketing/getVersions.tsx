4da5daee0a0d69a7020743a6a068ccde2957c4b5
Wasabi <admin@factorium1.de>
2025-10-19 10:43:24 +0200

    fix(typo): rename component

--- apps/web/src/components/marketing/getVersions.tsx
+++ apps/web/src/components/marketing/getVersions.tsx
@@ -3,8 +3,8 @@
 import fs from 'fs'
 import path from 'path'
 
-export async function getVersions() {
-  const folderPath = path.join(process.cwd(), 'src', 'assets', 'release')
+export async function getVersions(os: string) {
+  const folderPath = path.join(process.cwd(), 'src', 'assets', 'release', os)
   const files = fs.readdirSync(folderPath)
   const filteredFiles = files.map((file) => file.split('.')[0])
 
