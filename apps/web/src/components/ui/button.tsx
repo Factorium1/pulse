4e3e888655768f42b01dffc61d33311be168b97f
Nils Müller <92545107+Factorium1@users.noreply.github.com>
2025-10-29 15:48:11 +0100

    fix(shadcn): seperate file for shadcn (#15)

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import '@/assets/styles/shadcn.css'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
@@ -49,7 +50,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn('shadcn', buttonVariants({ variant, size, className }))}
       {...props}
     />
   )
--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -1,10 +1,11 @@
 import { cn } from '@/lib/utils'
+import '@/assets/styles/shadcn.css'
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="skeleton"
-      className={cn('bg-accent animate-pulse rounded-md', className)}
+      className={cn('shadcn bg-accent animate-pulse rounded-md', className)}
       {...props}
     />
   )
