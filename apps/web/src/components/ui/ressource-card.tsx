d3ba569dae34c0ee5b935b8f927413554cf20c81
Wasabi <admin@factorium1.de>
2025-11-06 08:42:42 +0100

    style: border and font

--- apps/web/src/components/ui/ressource-card.tsx
+++ apps/web/src/components/ui/ressource-card.tsx
@@ -12,10 +12,14 @@ const RessourceCard = ({
   link: string
 }) => {
   return (
-    <Link href={link} className="bg-indigo-200/20 rounded-lg flex-center flex-col p-4 gap-2">
+    <Link
+      href={link}
+      className="bg-indigo-200/20 rounded-lg flex justify-start items-center flex-col p-4 gap-2"
+    >
       <Icon className="text-indigo-500 text-2xl" />
-      <p className="text-2xl">{title}</p>
-      <p className="text-neutral-600 text-semibold text-center">{description}</p>
+      <p className="text-xl font-bold">{title}</p>
+      <div className="border-b border-2 w-full !border-indigo-500/40 rounded-full"></div>
+      <p className="text-neutral-600 text-semibold text-center text-md">{description}</p>
     </Link>
   )
 }
