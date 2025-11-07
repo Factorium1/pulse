a07ff2eeac1c98435f34201513a3937a871f10c5
Wasabi <admin@factorium1.de>
2025-11-07 15:05:48 +0100

    feat: create basic page and change studies page name

--- apps/web/src/app/(app)/studies/page.tsx
+++ apps/web/src/app/(app)/studies/page.tsx
@@ -1,10 +1,10 @@
 import SurveyCard from '@/components/ui/survey-card'
 
-const SurveyPage = () => {
+const StudiesPage = () => {
   return (
     <div className="flex flex-col mt-10 gap-6 px-4 md:px-8 lg:px-12">
       <div className="rounded-2xl bg-gradient-to-r from-primary to-indigo-600 text-primary-foreground px-6 py-8 shadow-md">
-        <div className="h1-bold">Umfragen - Eingang</div>
+        <div className="h1-bold">Eingang</div>
         {/* TODO: Filter erstellen */}
         <p className="mt-1 text-primary-foreground/90">
           Alle offenen Aufgaben aus deinen Studien — filterbar & klar gruppiert. 📬✨
@@ -83,4 +83,4 @@ const SurveyPage = () => {
   )
 }
 
-export default SurveyPage
+export default StudiesPage
---
+++ apps/web/src/app/(app)/studies/surveys/page.tsx
@@ -0,0 +1,14 @@
+const SurveyPage = () => {
+  return (
+    <div className="flex flex-col mt-10 gap-6 px-4 md:px-8 lg:px-12">
+      <div className="rounded-2xl bg-gradient-to-r from-primary to-indigo-600 text-primary-foreground px-6 py-8 shadow-md">
+        <div className="h1-bold">Umfragen - Eingang</div>
+        <p className="mt-1 text-primary-foreground/90">
+          Alle offenen Aufgaben aus deinen Studien — filterbar & klar gruppiert. 📬✨
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SurveyPage
