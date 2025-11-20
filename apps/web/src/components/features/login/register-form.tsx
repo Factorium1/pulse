7fdafa0dc2456c840d5f4ed3fde8d0c6c493b0ce
Wasabi <admin@factorium1.de>
2025-11-20 20:20:44 +0100

    feat(auth): user can register

--- apps/web/src/components/features/login/register-form.tsx
+++ apps/web/src/components/features/login/register-form.tsx
@@ -3,16 +3,59 @@
 import AuthSubmitButton from '@/components/features/login/auth-submit-button'
 import AuthSwitchHint from '@/components/features/login/auth-switch-hint'
 import InputField from '@/components/features/login/input-field'
+import { authClient } from '../../../../../../auth-client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 type RegisterFormProps = {
   onSwitchToLogin: () => void
 }
 
 const RegisterForm = ({ onSwitchToLogin }: RegisterFormProps) => {
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError(null)
+    setLoading(true)
+
+    const form = new FormData(e.currentTarget)
+    const name = form.get('name') as string
+    const email = form.get('email') as string
+    const password = form.get('password') as string
+    const confirmPassword = form.get('confirmPassword') as string
+
+    if (password !== confirmPassword) {
+      setError('Die Passwörter stimmen nicht überein.')
+      return
+    }
+
+    const { error } = await authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+        callbackURL: '/dashboard',
+      },
+      {
+        onSuccess() {
+          router.push('/dashboard')
+        },
+      },
+    )
+
+    if (error) {
+      setError(error?.message ?? null)
+      setLoading(false)
+    }
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <p className="h3-bold">Erstelle dein Pulse-Konto</p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleRegister}>
         <InputField
           label="Name"
           name="name"
@@ -48,6 +91,11 @@ const RegisterForm = ({ onSwitchToLogin }: RegisterFormProps) => {
         actionLabel="Anmelden"
         onAction={onSwitchToLogin}
       />
+      {error && (
+        <p className="text-xs font-semibold text-center bg-red-400/40 text-red-600 px-5 py-2 rounded-full mt-2">
+          {error}
+        </p>
+      )}
     </div>
   )
 }
