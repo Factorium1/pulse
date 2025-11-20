173fdae0e58ba1e84f86df355edf8239bfafb0cc
Wasabi <admin@factorium1.de>
2025-11-20 19:24:04 +0100

    feat(auth): login

--- apps/web/src/components/features/login/login-form.tsx
+++ apps/web/src/components/features/login/login-form.tsx
@@ -3,12 +3,40 @@
 import AuthSubmitButton from '@/components/features/login/auth-submit-button'
 import AuthSwitchHint from '@/components/features/login/auth-switch-hint'
 import InputField from '@/components/features/login/input-field'
+import { authClient } from '../../../../../../auth-client'
+import { useState } from 'react'
 
 type LoginFormProps = {
   onSwitchToRegister: () => void
 }
 
 const LoginForm = ({ onSwitchToRegister }: LoginFormProps) => {
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setError(null)
+    setLoading(true)
+
+    const form = new FormData(e.currentTarget)
+    const email = form.get('email') as string
+    const password = form.get('password') as string
+    const rememberMe = form.get('rememberMe') === 'on'
+
+    const { error } = await authClient.signIn.email({
+      email,
+      password,
+      callbackURL: '/dashboard',
+      rememberMe,
+    })
+
+    if (error) {
+      setError(error?.message ?? null)
+      setLoading(false)
+    }
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <p className="h3-bold">Melde dich bei Pulse an</p>
@@ -46,6 +74,11 @@ const LoginForm = ({ onSwitchToRegister }: LoginFormProps) => {
         actionLabel="Registrieren"
         onAction={onSwitchToRegister}
       />
+      {error && (
+        <p className="text-xs font-semibold text-center bg-red-400/40 text-red-600 px-5 py-2 rounded-full mt-2">
+          {error}
+        </p>
+      )}
     </div>
   )
 }
