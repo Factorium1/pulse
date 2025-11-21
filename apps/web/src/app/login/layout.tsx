48033a43dba24dbb898a984808b271938a4ad565
Wasabi <admin@factorium1.de>
2025-11-21 07:03:11 +0100

    feat(auth): secure routes

--- apps/web/src/app/login/layout.tsx
+++ apps/web/src/app/login/layout.tsx
@@ -1,7 +1,17 @@
-export default function RootLayout({
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { auth } from '../../../../../auth'
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  if (session) {
+    redirect('/dashboard')
+  }
   return <main className="flex-1 ">{children}</main>
 }
