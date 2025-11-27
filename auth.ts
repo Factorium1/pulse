cf272d84789314ac1fce909bda56325060c30a8d
Wasabi <admin@factorium1.de>
2025-11-27 10:27:15 +0100

    package: update prisma

--- auth.ts
+++ auth.ts
@@ -1,12 +1,24 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
-// If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-const prisma = new PrismaClient()
+const rawUrl = process.env.DATABASE_URL
+if (!rawUrl) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+const connectionString = rawUrl.replace('postgresql://', 'postgres://')
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({ adapter } as any) // TypeScript workaround
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: 'postgresql', // or "mysql", "postgresql", ...etc
+    provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
