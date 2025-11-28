f7ad1ac5ee54990a5687667a94b17aace071711b
Wasabi <admin@factorium1.de>
2025-11-28 09:37:49 +0100

    feat(questionExecuter): manage question array

---
+++ apps/web/src/components/features/create/question-executer.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from 'react'
+import QuestionCard from './question-card'
+
+type QuestionType = {
+  id: string
+  type: 'freetext' | 'multiple-choice' | 'single-choice' | 'rating'
+  title: string
+  description?: string
+  answerChoices?: number
+  questionChoices?: number
+  question?: string[]
+}
+
+const QuestionExecuter = () => {
+  const [questions, setQuestions] = useState<QuestionType[]>([
+    { id: crypto.randomUUID(), type: 'freetext', title: '' },
+  ])
+
+  function addQuestion({
+    type,
+    title,
+    description,
+    answerChoices,
+    questionChoices,
+    question,
+  }: QuestionType) {
+    setQuestions((prev) => [
+      ...prev,
+      {
+        id: crypto.randomUUID(),
+        type: type,
+        title: title,
+        description: description,
+        answerChoices: answerChoices,
+        questionChoices: questionChoices,
+        question: question,
+      },
+    ])
+  }
+
+  function removeQuestion(id: string) {
+    setQuestions((prev) => prev.filter((question) => question.id !== id))
+  }
+
+  return (
+    <div className="w-full flex-center flex-col gap-4 p-4 border border-border/70 rounded-lg bg-background/70">
+      {questions.map((question) => (
+        <QuestionCard key={question.id} />
+      ))}
+    </div>
+  )
+}
+
+export default QuestionExecuter
