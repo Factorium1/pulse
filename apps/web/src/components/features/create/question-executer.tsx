0ae944035c698d83974c49af1195939a6c4d3f33
Wasabi <admin@factorium1.de>
2025-11-29 16:16:55 +0100

    style(questionCard): if no question is available

--- apps/web/src/components/features/create/question-executer.tsx
+++ apps/web/src/components/features/create/question-executer.tsx
@@ -16,7 +16,7 @@ const QuestionExecuter = ({
   return (
     <div className="w-full flex-center flex-col gap-4 p-4 border border-border/70 rounded-lg bg-background/70">
       {questions.length === 0 ? (
-        <div className="flex-center flex-col gap-2">
+        <div className="flex-center flex-col gap-2 px-5 lg:px-20 py-10">
           <CheckCircle className="h-8 w-8 text-emerald-500" />
           <p className="text-sm text-muted-foreground text-center">
             Es wurden noch keine Fragen hinzugefügt. Klicke auf "Frage hinzufügen", um zu beginnen
