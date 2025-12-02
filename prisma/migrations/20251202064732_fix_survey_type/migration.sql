59f86e348af8834af5ab2e8bf22f3b9f08bc65c7
Wasabi <admin@factorium1.de>
2025-12-02 07:47:59 +0100

    refactor: migrate db

---
+++ prisma/migrations/20251202064732_fix_survey_type/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [ONE_SHOT,MULTI_DAY] on the enum `SurveyType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "SurveyType_new" AS ENUM ('SHORT', 'LONG');
+ALTER TABLE "public"."Survey" ALTER COLUMN "type" DROP DEFAULT;
+ALTER TABLE "Survey" ALTER COLUMN "type" TYPE "SurveyType_new" USING ("type"::text::"SurveyType_new");
+ALTER TYPE "SurveyType" RENAME TO "SurveyType_old";
+ALTER TYPE "SurveyType_new" RENAME TO "SurveyType";
+DROP TYPE "public"."SurveyType_old";
+ALTER TABLE "Survey" ALTER COLUMN "type" SET DEFAULT 'SHORT';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Survey" ALTER COLUMN "type" SET DEFAULT 'SHORT';
