7dfbf3fd253f6aa0cde9b3c9af488f10abe03c35
Wasabi <admin@factorium1.de>
2026-01-12 08:16:55 +0100

    feat(taskManager): get all active surveys for user

--- apps/web/src/app/(app)/studies/actions.ts
+++ apps/web/src/app/(app)/studies/actions.ts
@@ -1,3 +1,43 @@
 'use server'
 
-export async function getParticipantSurveys() {}
+import { headers } from 'next/headers'
+import { auth } from '../../../../../../auth'
+import { prisma } from '../../../../../../prisma'
+import { SurveyType } from '@prisma/client'
+
+export async function getParticipantSurveys() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  const userId = session?.user?.id
+  if (!userId) {
+    return 'Unauthorized'
+  }
+
+  try {
+    const surveys = prisma.survey.findMany({
+      where: {
+        surveyParticipations: {
+          userId,
+          status: 'ACTIVE',
+          type: SurveyType.LONG,
+        },
+      },
+      select: {
+        title: true,
+        shortLabel: true,
+        emoji: true,
+        surveyBlock: true,
+      },
+    })
+
+    if (!surveys) {
+      return 'No Surveys found'
+    }
+
+    return surveys
+  } catch (err) {
+    return String(err)
+  }
+}
