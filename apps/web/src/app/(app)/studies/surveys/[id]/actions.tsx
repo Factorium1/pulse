1b15013244c47ea2213f190a85cb43365bd4af76
Wasabi <admin@factorium1.de>
2026-01-14 12:43:40 +0100

    feat(survey): getBlock from db

--- apps/web/src/app/(app)/studies/surveys/[id]/actions.tsx
+++ apps/web/src/app/(app)/studies/surveys/[id]/actions.tsx
@@ -1,3 +1,40 @@
 'use server'
 
-export async function getBlock(id: string) {}
+import { headers } from 'next/headers'
+import { auth } from '../../../../../../../../auth'
+import { prisma } from '../../../../../../../../prisma'
+
+export async function getBlock(id: string) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  const userId = session?.user?.id
+  if (!userId) {
+    return { ok: false, message: 'Unauthorized' }
+  }
+
+  try {
+    const block = await prisma.surveyblock.findFirst({
+      where: {
+        id,
+        survey: {
+          participants: {
+            some: {
+              userId,
+              status: 'ACTIVE',
+            },
+          },
+        },
+      },
+    })
+
+    if (block.survey?.participants?.length < 0 || block === null) {
+      return { ok: false, message: 'Unauthorized' }
+    }
+
+    return { ok: true, block: block }
+  } catch (err) {
+    return { ok: false, message: 'Network Error', error: String(err) }
+  }
+}
