bbe9b8a0f911268b154787a4c71dae783e234a62
Wasabi <admin@factorium1.de>
2026-01-14 18:45:09 +0100

    feat(survey): handle back and next button

--- apps/web/src/components/features/studies/surveys/manage-page.tsx
+++ apps/web/src/components/features/studies/surveys/manage-page.tsx
@@ -4,15 +4,34 @@ import SingleChoice from './single-choice'
 import { Progress } from '@/components/ui/progress'
 import { useState } from 'react'
 import { Question } from '@prisma/client'
+import { redirect } from 'next/navigation'
 
 const ManagePage = ({ questions }: { questions: Question[] }) => {
   const [questionLength, setQuestionLength] = useState<number>(1)
+  const [question, setQuestion] = useState<Question>(questions[questionLength - 1])
 
   const total = questions.length
   const progress = total > 0 ? (questionLength / total) * 100 : 0
 
   const backHidden = questionLength === 1
   const isLast = total > 0 && questionLength === total
+
+  async function handleNext() {
+    if (questionLength === total) {
+      // await
+      redirect('/studies')
+    }
+
+    setQuestion(questions[questionLength])
+  }
+
+  async function handleBack() {
+    if (questionLength === 0) {
+      return
+    }
+
+    setQuestion(questions[questionLength - 1])
+  }
 
   return (
     <div className="w-full">
@@ -25,11 +44,15 @@ const ManagePage = ({ questions }: { questions: Question[] }) => {
           <SingleChoice />
           <div className="flex-between w-full">
             <button
+              onClick={() => handleNext()}
               className={`text-sm text-muted-foreground font-medium cursor-pointer border-2 px-6 py-2 rounded-md ${backHidden ? 'invisible' : ''}`}
             >
               Zurück
             </button>
-            <button className="bg-primary text-primary-foreground px-6 py-2 rounded-md text-sm font-medium cursor-pointer">
+            <button
+              onClick={() => handleBack()}
+              className="bg-primary text-primary-foreground px-6 py-2 rounded-md text-sm font-medium cursor-pointer"
+            >
               {isLast ? 'Absenden' : 'Weiter'}
             </button>
           </div>
