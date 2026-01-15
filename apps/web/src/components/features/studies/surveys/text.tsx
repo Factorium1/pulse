dec1b92994ea51ff58ce0c856e28471bfc0828c9
Wasabi <admin@factorium1.de>
2026-01-15 12:12:24 +0100

    feat(survey): add logic to text component

--- apps/web/src/components/features/studies/surveys/text.tsx
+++ apps/web/src/components/features/studies/surveys/text.tsx
@@ -1,5 +1,35 @@
-const TextQuestion = () => {
-  return <div className="">Text</div>
+import { Question } from '@prisma/client'
+
+const TextQuestion = ({
+  question,
+  value,
+  onChange,
+}: {
+  question: Question
+  value: string
+  onChange: (value: string) => void
+}) => {
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <div className="flex-start gap-2 flex-col">
+        <h2 className="h2-bold">{question.title}</h2>
+        {question.description && (
+          <p className="text-muted-foreground text-sm">{question.description}</p>
+        )}
+      </div>
+      <div className="flex-start gap-4 flex-col w-full">
+        <textarea
+          name="text"
+          id="text"
+          rows={10}
+          placeholder="Text eingeben..."
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="w-full border-2 border-accent rounded-lg p-2 resize-none overflow-hidden"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default TextQuestion
